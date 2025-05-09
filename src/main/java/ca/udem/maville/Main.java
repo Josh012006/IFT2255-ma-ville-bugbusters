29ee0fb1a978593519d316acbf9db2ecdba49689
Roxanne Cabedoce <roxanne.cabedoce@umontreal.ca>
2025-05-09 15:31:08 -0400

    modif

--- src/main/java/ca/udem/maville/Main.java
+++ src/main/java/ca/udem/maville/Main.java
@@ -2,6 +2,6 @@ package ca.udem.maville;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! lol");
     }
 }
