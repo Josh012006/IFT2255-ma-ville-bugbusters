f7a8ce285b8b7431743a790f101c77bec5106b62
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-07-16 19:15:07 -0400

    fix: Fixed the precisions in ResidentDAO for findToNotify method

--- backend/src/main/java/ca/udem/maville/server/dao/files/users/ResidentDAO.java
+++ backend/src/main/java/ca/udem/maville/server/dao/files/users/ResidentDAO.java
@@ -8,9 +8,9 @@ public class ResidentDAO {
     // Todo: Une méthode findAll() qui renvoie une liste de tous les résidents dans la base de
     //  données.
 
-    // Todo: Une méthode findToNotify(String quartier, String rue) qui renvoie tous les résidents
-    //  de la base de données qui ont le quartier présent dans leur liste de quartiers auxquels ils osnt interessés
-    //  ou bien la rue présente dans leur liste de rues auxquelles ils sont abonnés.
+    // Todo: Une méthode findToNotify(String quartier, String[] rues) qui renvoie tous les résidents
+    //  de la base de données qui ont le quartier présent dans leur liste de quartiers auxquels ils sont interessés
+    //  ou bien au moins l'une des rues présente dans leur liste de rues auxquelles ils sont abonnés.
 
     // Todo: Une méthode save(Resident resident) qui permet de sauvegarder un résident dans la base de données.
 }
