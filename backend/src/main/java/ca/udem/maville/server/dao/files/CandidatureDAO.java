6515738b8509cef39be6dcd6e1ac1088dc201a4b
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-07-16 18:38:29 -0400

    feat: Add the methods that must be impelmented in the DAO files for more guidance

--- backend/src/main/java/ca/udem/maville/server/dao/files/CandidatureDAO.java
+++ backend/src/main/java/ca/udem/maville/server/dao/files/CandidatureDAO.java
@@ -1,5 +1,20 @@
 package ca.udem.maville.server.dao.files;
 
 public class CandidatureDAO {
-    // Todo: Don't forget to allow createdAt et updatedAt
+
+    // Todo: Une méthode findAll() qui renvoie une liste de toutes les candidatures dans la base de données ayant
+    //  un statut "en attente".
+
+    // Todo: Une méthode findById(ObjectId id) qui renvoie un élément de type model Candidature qui représente la
+    //  candidature de la base de données ayant l'id donné.
+
+    // Todo: Une méthode findPrestataireCandidatures(ObjectId userId) qui renvoie toutes candidatures du prestataire
+    //  avec id userId, c'est-à-dire dont le champ prestataire est userId.
+
+    // Todo: Une méthode save(Candidature candidature) qui enregistre la candidature dans la base de données. Elle ne
+    //  renvoie rien.
+
+    // Todo: Une méthode delete(ObjectId id) qui supprime la candidature de la base de données ayant l'id donné. Elle
+    //  ne renvoie rien.
+
 }
--- backend/src/main/java/ca/udem/maville/server/dao/files/SignalementDAO.java
+++ backend/src/main/java/ca/udem/maville/server/dao/files/SignalementDAO.java
@@ -1,5 +1,16 @@
 package ca.udem.maville.server.dao.files;
 
 public class SignalementDAO {
-    // Todo: Don't forget to allow createdAt et updatedAt
+
+    // Todo: Une méthode findById(ObjectId id) qui renvoie un élément de type model Signalement dont l'id est
+    //  celui donné.
+
+    // Todo: Une méthode findAll() qui renvoie la liste de tous les signalements de la base de données ayant un statut
+    //  "en attente".
+
+    // Todo: Une méthode findResidentSignalements(ObjectId userId) qui renvoie la liste de tous les signalements de la
+    //  base de données appartenant au résident dont l'id est userId. Autrement dit, leur champ resident porte la valeur userId.
+
+    // Todo: Une méthode save(Signalement signalement) qui enregistre un signalement dans la base de données.
+
 }
