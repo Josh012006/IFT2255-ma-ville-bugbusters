541d6443ced06f7aa576b6f4760e9a7eb3340e6d
lalllou <lallia.diakite@umontreal.ca>
2025-07-19 18:16:32 -0400

    Les classes dans Users

--- backend/src/main/java/ca/udem/maville/server/dao/files/users/NotificationDAO.java
+++ backend/src/main/java/ca/udem/maville/server/dao/files/users/NotificationDAO.java
@@ -1,14 +1,49 @@
 package ca.udem.maville.server.dao.files.users;
 
+import ca.udem.maville.server.dao.config.MongoConfig;
+import ca.udem.maville.server.models.users.Notification;
+import dev.morphia.query.experimental.filters.Filters;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
 public class NotificationDAO {
 
     // Todo: Une méthode findUserNotifications(ObjectId userId) qui prend l'id d'un utilisateur et
     //  renvoie une liste contenant toutes ses notifications.
+    /**
+     * Récupère toutes les notifications destinées à un utilisateur
+     */
+    public List<Notification> findUserNotifications(ObjectId userId){
+        return MongoConfig.getDatastore()
+                .find(Notification.class)
+                .filter(Filters.eq("destinataire", userId))
+                .iterator()
+                .toList();
+    }
 
     // Todo: Une méthode save(Notification notification) qui prend un élément de type model Notification
     //  et le sauvegarde dans la base de données. Elle ne renvoie rien.
 
+/**
+     * Sauvegarde ou met à jour une notification
+     */
+    public void save(Notification notication){
+        MongoConfig.getDatastore().save(notification);
+    }
+
     // Todo: Une méthode findById(ObjectId id) qui renvoie un élément de type model Notification
     //  qui représente la notification avec un tel id dans la base de données.
+
+    /**
+     * Récupère une notification par son ID
+     */
+    public Notification findById(ObjectId id){
+        return MongoConfig.getDatastore()
+                .find(Notification.class)
+                .filter(Filters.eq("_id", id))
+                .first();
+
+    }
 
 }
--- backend/src/main/java/ca/udem/maville/server/dao/files/users/PrestataireDAO.java
+++ backend/src/main/java/ca/udem/maville/server/dao/files/users/PrestataireDAO.java
@@ -1,16 +1,65 @@
 package ca.udem.maville.server.dao.files.users;
 
+import ca.udem.maville.server.dao.config.MongoConfig;
+import ca.udem.maville.server.models.users.Prestataire;
+import dev.morphia.query.experimental.filters.Filters;
+import dev.morphia.query.experimental.filters.Filter;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
 public class PrestataireDAO {
 
     // Todo: Une méthode findById(ObjectId id) qui renvoie un élément de type model Prestataire
     //  qui représente le prestataire avec un tel id dans la base de données.
+    /**
+     * Récupère un prestataire par son ID
+     */
+    public Prestataire findById(ObjectId id){
+        return MongoConfig.getDatastore()
+                .find(Prestataire.class)
+                .filter(Filters.eq("_id", id))
+                .first();
+    }
 
     // Todo: Une méthode findAll() qui renvoie une liste de tous les prestataires dans la base de
     //  données.
+    /**
+     * Récupère tous les prestataires
+     */
+    public List<Prestataire> findAll() {
+        return MongoConfig.getDatastore()
+                .find(Prestataire.class)
+                .iterator()
+                .toList();
+    }
 
     // Todo: Une méthode findToNotify(String quartier, String typeTravail) qui renvoie tous les prestataires
     //  de la base de données qui ont le quartier présent dans leur liste de quartiers couverts ou bien le typeTravail
     //  présent dans leur liste de types de travaux couverts.
 
+    /**
+     * Récupère tous les prestataires à notifier selon le quartier ou le type de travaux
+     */
+    public List<Prestataire> findToNotify(String quartier, String typeTravail) {
+        return MongoConfig.getDatastore()
+                .find(Prestataire.class)
+                .filter(
+                    Filters.or(
+                        Filters.eq("quartiersCouverts", quartier),
+                        Filters.eq("typesTravauxCouverts", typeTravail)
+                    )
+                )
+                .iterator()
+                .toList();
+    }
+
     // Todo: Une méthode save(Prestataire prestataire) qui permet de sauvegarder un prestataire dans la base de données.
+
+    /**
+     * Sauvegarde ou met à jour un prestataire
+     */
+    public void save(Prestataire prestataire) {
+        MongoConfig.getDatastore().save(prestataire);
+    }
 }
