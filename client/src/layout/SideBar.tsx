6bbfad857d7ea344f3c76ac1b83f5f68d45f711e
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-07-25 22:01:20 -0400

    feat: Implement the 404 not-found page

--- client/src/layout/SideBar.tsx
+++ client/src/layout/SideBar.tsx
@@ -1,5 +1,8 @@
 import type { ReactNode } from "react";
-import { useAppSelector } from "../redux/store";
+import { type AppDispatch, useAppSelector } from "../redux/store";
+import MyLink from "../components/MyLink";
+import { useDispatch } from "react-redux";
+import { loginInfos, loginType } from "../redux/features/authSlice";
 
 
 /**
@@ -12,10 +15,18 @@ import { useAppSelector } from "../redux/store";
 export default function SideBar({children, show} : {children: ReactNode, show: boolean}) {
     const userType : string | null = useAppSelector((state) => state.auth.userType);
 
+    const dispatch = useDispatch<AppDispatch>();
+
+    const handleLogout = () => {
+        dispatch(loginInfos(null));
+        dispatch(loginType(null));
+    }
+
     return(
         <main className="row">
-            <aside className={`${show? "d-block" : "d-none"} col-12 col-lg-2 d-lg-block h-100 z-1 z-lg-0 bg-light`}>
+            <aside className={`${show? "d-block" : "d-none"} col-12 col-lg-2 d-lg-block h-100 z-1 z-lg-0 bg-light d-flex flex-column justify-content-center align-items-center`}>
                 {userType}
+                <MyLink to="/auth" className="" onClick={handleLogout}>Revenir à l'authentification</MyLink>
             </aside>
             <div className="col-12 col-lg-10 z-0">{ children }</div>
         </main>
