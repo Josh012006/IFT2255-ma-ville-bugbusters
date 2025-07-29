5fba7a0d11a834dfc149b94d96bc2fd45feeb81f
Roxanne Cabedoce <roxanne.cabedoce@umontreal.ca>
2025-07-29 14:46:59 -0400

    Ajout des imports necessaire et le style modal

--- client/src/pages/prestataire/PrestataireCandidaturePage.tsx
+++ client/src/pages/prestataire/PrestataireCandidaturePage.tsx
@@ -1,5 +1,8 @@
-import { useParams } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import {Button,Card,CardContent,Typography,Modal,TextField,Box,CardActions,} from "@mui/material";
+import useRequest from "../hooks/useRequest";
+import type { Candidature } from "../types/Candidature";
 
 
 /**
@@ -8,7 +11,19 @@ import { useParams } from "react-router-dom";
  * Le prestataire a aussi la possibilité de modifier ou de supprimer la candidature tant que son
  * statut n'est pas marquée comme 'vue' par le STPM.
  * @returns ReactNode
- */
+ */ 
+
+const modalStyle = {
+    position: "absolute" as const,
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 500,
+    bgcolor: "background.paper",
+    p: 4,
+    borderRadius: 2,
+};
+
 export default function PrestataireCandidaturePage() {
     const candidatureId = useParams().id;
 
