b3dbe42ca63c8af08b8eb1dec52704f8a59e9f2c
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-07-29 13:25:19 -0400

    feat: Finished implementation of ResidentSignalements.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -81,7 +81,7 @@ function App() {
                         <Route index element={<ChoiceOptionSignalementPage />} />
                         <Route path="new" element={<NewSignalementPage />} />
                         <Route path="list" element={<ResidentSignalementsPage />} />
-                        <Route path=":id" element={<ResidentSignalementPage />} />   {/* On inclut aussi dans la page la modification du signalement */}
+                        <Route path=":id" element={<ResidentSignalementPage />} />   {/* On inclut aussi dans la page, la modification du signalement */}
                     </Route>
                 </Route>
 
